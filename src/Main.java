282bd0bc9e41d193faeefea4974c3e60f86de83a
Yuriy <wildgrowth1@gmail.com>
2018-03-06 01:40:05 +0200

    My first changes
    Signed-off-by: Yuriy <wildgrowth1@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 /**
  * Created by WildGrowth on 06.03.2018.
  */
-public class main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Yo! My friends");
+        System.out.println("Yo! My friends. I try tu push my project on Git");
     }
 }
