b02a7a930dcd0a9b52c00d62327a344f2bcf393b
Yuriy <wildgrowth1@gmail.com>
2018-03-06 02:10:32 +0200

    My first changes
    Signed-off-by: Yuriy <wildgrowth1@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Yo! My friends. I try tu push my project on Git. Main");
+        System.out.println("Yo! My friends. I try tu push my project on Git. Main dfg");
     }
 }
